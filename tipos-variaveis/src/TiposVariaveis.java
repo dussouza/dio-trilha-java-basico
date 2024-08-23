4d6a0f1c6550eea588c5a8180d04cfefa1c08be4
dusssouza <eduardosouza2@hotmail.com>
2024-08-23 11:20:01 -0300

    Tipos-de-Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Tipos primitivos
+        // Estudar a classe String que representa texto na aplicação
+
+        String meuNome = "Eduardo Silva de Souza";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
