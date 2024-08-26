f78050744de3d299a954d556ec7bf8ca2a193314
dusssouza <eduardosouza2@hotmail.com>
2024-08-26 09:36:49 -0300

    repeticoes-sintaxes-java

---
+++ AboutMe/src/AboutMe.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        //Criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a sua altura: ");
+        double altura = scanner.nextDouble();
+
+        //Retornando o preenchimento ao usuário
+        System.out.println("Olá, me chamo: " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+
+    }
+}
